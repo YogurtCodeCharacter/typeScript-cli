76eb0aa8118a4e1b9358d269bb5c889769f65472
zongxiaobai <17682309829@163.com>
2020-08-24 23:47:50 +0800

    add:ts init

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
+import React from 'react'
 import { Layout, Menu, Icon } from 'antd';
 import styles from './index.less';
 
 const { Header, Content, Footer, Sider } = Layout;
 
 interface IProps {
-  color: string,
+  color?: string,
   size?: string,
 }
 interface IState {
@@ -15,47 +15,47 @@ class Index extends React.Component<IProps, IState> {
   public state = {
     count: 1,
   }
-  public render () {
+  public render() {
     return (
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className={styles.logo} />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span className="nav-text">nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span className="nav-text">nav 34</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="user" />
+              <span className="nav-text">nav 4</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
         <Layout>
-    <Sider
-      breakpoint="lg"
-      collapsedWidth="0"
-      onBreakpoint={broken => {
-        console.log(broken);
-      }}
-      onCollapse={(collapsed, type) => {
-        console.log(collapsed, type);
-      }}
-    >
-      <div className={styles.logo} />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1">
-          <Icon type="user" />
-          <span className="nav-text">nav 1</span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="video-camera" />
-          <span className="nav-text">nav 2</span>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="upload" />
-          <span className="nav-text">nav 34</span>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Icon type="user" />
-          <span className="nav-text">nav 4</span>
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout>
-      <Header style={{ background: '#fff', padding: 0 }} />
-      <Content style={{ margin: '24px 16px 0' }}>
-        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>content</div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant dUED</Footer>
-    </Layout>
-  </Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>content</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant dUED</Footer>
+        </Layout>
+      </Layout>
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const NODE_MODULES = /node_modules/;
 
 module.exports = {
   mode: 'development', // 开发模式
-  entry: path.resolve(__dirname, './src/main.js'),    // 入口文件
+  entry: path.resolve(__dirname, './src/index.js'),    // 入口文件
   output: {
     filename: '[name].[hash:8].js',      // 打包后的文件名称
     path: path.resolve(__dirname, './dist')  // 打包后的目录
